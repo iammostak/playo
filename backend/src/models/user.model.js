f528b38ca1a6cdbca2ee800a584d03842a581ac8
iammostak <mdmostafijur85@gmail.com>
2022-12-20 21:49:00 +0530

    user model

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,11 @@
+const { model, Schema } = require("mongoose");
+
+const UserSchema = new Schema({
+   username: { type: String, required: true, unique: true },
+   email: { type: String, required: true, unique: true },
+   password: { type: String, required: true },
+});
+
+const UserModel = model("user", UserSchema);
+
+module.exports = UserModel;
