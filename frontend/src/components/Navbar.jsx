165c5904a7a2612d5f6b540fc5c13a153272b4ee
iammostak <mdmostafijur85@gmail.com>
2022-12-21 00:22:40 +0530

    navbar created

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,36 @@
+import { Button, Flex, Heading, HStack } from "@chakra-ui/react";
+import { NavLink } from "react-router-dom";
+
+function Navbar() {
+   return (
+      <Flex
+         py={4}
+         px={10}
+         w={"full"}
+         bg={"white"}
+         boxShadow={"md"}
+         align={"center"}
+         justify={"space-between"}
+      >
+         <Heading size={"lg"} as={NavLink} to="/">
+            🅿🅻🅰🆈🅾
+         </Heading>
+         <HStack spacing={4}>
+            <Button as={NavLink} to="/login" px={7} borderRadius={"3xl"}>
+               Login
+            </Button>
+            <Button
+               as={NavLink}
+               to="/signup"
+               px={7}
+               colorScheme={"blue"}
+               borderRadius={"3xl"}
+            >
+               Signup
+            </Button>
+         </HStack>
+      </Flex>
+   );
+}
+
+export default Navbar;
