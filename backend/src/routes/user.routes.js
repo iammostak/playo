713ef6cd67590f083fc7085531f1fa9baf08c20a
iammostak <mdmostafijur85@gmail.com>
2022-12-20 22:44:42 +0530

    login route added

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -16,27 +16,53 @@ app.post("/register", async (req, res) => {
 
    try {
       if (user && username === user.username && email === user.email) {
-         return res.status(400).send({
+         res.status(400).send({
             user,
             message: "user already exists, try to login",
          });
       } else if (user && username === user.username && email !== user.email) {
-         return res.status(400).send({
+         res.status(400).send({
             user,
             message: "username already in use",
          });
       } else if (user && username !== user.username && email === user.email) {
-         return res.status(400).send({
+         res.status(400).send({
             user,
             message: "email has already been used",
          });
       } else {
          user = new UserModel({ username, email, password });
          await user.save();
-         res.send({ user, message: "account created successfully" });
+         res.send({ user, message: "account created successfully!" });
       }
    } catch (err) {
-      return res.status(400).send("registration failed, please try again");
+      res.status(400).send({message: "registration failed, please try again"});
+   }
+});
+
+app.post("/login", async (req, res) => {
+   const { username, password } = req.body;
+
+   const user = await UserModel.findOne({ username, password });
+
+   try {
+      if (!user) {
+         res.status(400).send({message: "invalid credential!"});
+      } else {
+         const token = jwt.sign(
+            { username: user.username, email: user.email },
+            secretKey,
+            { expiresIn: "7 days" }
+         );
+
+         res.send({
+            user,
+            token,
+            message: "user logged in successfully",
+         });
+      }
+   } catch (err) {
+      res.status(400).send("login failed, please try again");
    }
 });
 
