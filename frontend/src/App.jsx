79062c7fb6945ffb68c4d5b2140e65be573e3a69
iammostak <mdmostafijur85@gmail.com>
2022-12-21 00:22:50 +0530

    navbar added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,18 @@
 import { Container } from "@chakra-ui/react";
+import Navbar from "./components/Navbar";
 import AllRoutes from "./routes/AllRoutes";
 
 function App() {
    return (
-      <Container m={0} p={0} maxW={"container"} minH={"100vh"} centerContent>
+      <Container
+         m={0}
+         p={0}
+         minH={"100vh"}
+         maxW={"container"}
+         bg={"whitesmoke"}
+         centerContent
+      >
+         <Navbar />
          <AllRoutes />
       </Container>
    );
