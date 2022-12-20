b9ea4bee5bb33a6536e77c89ddf2ad9bfc15373c
iammostak <mdmostafijur85@gmail.com>
2022-12-20 23:02:31 +0530

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import { Container, Heading } from "@chakra-ui/react";
+
+function App() {
+   return (
+      <Container m={0} p={0} maxW={"container"} minH={"100vh"} centerContent>
+         <Heading>Playo App</Heading>
+      </Container>
+   );
+}
+
+export default App;
