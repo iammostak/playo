f33b0c094caefd41e04f3f962dc778d690fd4d4f
iammostak <mdmostafijur85@gmail.com>
2022-12-21 15:17:31 +0530

    event routes

---
+++ backend/src/routes/event.routes.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const EventModel = require("../models/event.model");
+
+const app = express.Router();
+
+app.get("/", async (req, res) => {
+   try {
+      const event = await EventModel.find().populate("organizer");
+      res.send({ event });
+   } catch (err) {
+      res.send({ message: err });
+   }
+});
+
+app.post("/post", async (req, res) => {
+   const data = req.body;
+   try {
+      const event = new EventModel(data);
+      await event.save();
+      res.send({ event, message: "Event posted successfully" });
+   } catch (err) {
+      res.status(400).send({ message: err });
+   }
+});
+
+app.get("/:id", async (req, res) => {
+   const { id } = req.params;
+
+   try {
+      const event = await EventModel.findById(id).populate("organizer");
+      res.send({ event });
+   } catch (err) {
+      res.status(400).send({ message: err });
+   }
+});
+
+module.exports = app;
