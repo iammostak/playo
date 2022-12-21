88d197a9b611488f7ed61ced85ee424ed42698aa
iammostak <mdmostafijur85@gmail.com>
2022-12-21 20:21:23 +0530

    event lists rendering done

--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -1,10 +1,59 @@
-import { Box, Heading } from "@chakra-ui/react";
+import {
+   Box,
+   Button,
+   Flex,
+   Heading,
+   HStack,
+   Input,
+   Select,
+   Tag,
+   Text,
+   VStack,
+} from "@chakra-ui/react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import EventCard from "../components/EventCard";
+import { getEventsAction } from "../store/event/event.actions";
 
 function Home() {
+   const { events } = useSelector((store) => store.event);
+   const dispatch = useDispatch();
+
+   useEffect(() => {
+      dispatch(getEventsAction());
+   }, []);
+
    return (
-      <Box>
-         <Heading>Home Page!</Heading>
-      </Box>
+      <VStack my={7} justify={"stretch"} spacing={5}>
+         <Heading
+            mb={3}
+            pb={4}
+            align={"center"}
+            color={"blue.500"}
+            fontFamily={"Helvetica"}
+            fontWeight={800}
+            letterSpacing={0.5}
+            borderBottom={"1px solid"}
+            borderColor={"blackAlpha.100"}
+         >
+            All Events
+         </Heading>
+         <HStack pb={4} w={690}>
+            <Input bg={"white"} placeholder="🔍 Search for an event..." />
+            <Button w={40} colorScheme={"blue"}>
+               Search
+            </Button>
+            <Select bg={"white"}>
+               <option value="">Filter by Game</option>
+               <option value="Football">Football</option>
+               <option value="Cricket">Cricket</option>
+               <option value="Badminton">Badminton</option>
+            </Select>
+         </HStack>
+         {events.map((event) => (
+            <EventCard key={event._id} {...event} />
+         ))}
+      </VStack>
    );
 }
 
