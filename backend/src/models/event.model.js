f56cbce1ff04832090dfa5599e89f11d52d9bc01
iammostak <mdmostafijur85@gmail.com>
2022-12-21 15:17:18 +0530

    event model

---
+++ backend/src/models/event.model.js
@@ -0,0 +1,13 @@
+const { model, Schema } = require("mongoose");
+
+const EventSchema = new Schema({
+   title: { type: String, required: true },
+   description: { type: String, required: true },
+   startAt: { type: String, required: true },
+   playersCount: { type: Number, required: true },
+   organizer: { type: Schema.Types.ObjectId, ref: "user" },
+});
+
+const EventModel = model("event", EventSchema);
+
+module.exports = EventModel;
